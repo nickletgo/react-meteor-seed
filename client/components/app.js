18b1784dcfa3f7239ecc248ae82088fe59056f77
nickletgo <nickletgo@gmail.com>
2016-09-06 22:51:41 -0400

    1. Inited eslint configuration by running eslint --init. It added eslint and eslint-plugin-react as well as .eslintrc.json configuration file to this project 2. Added react and react-dom packages 3. Added the main react component <App /> and modified main.js and main.html to render this project

---
+++ client/components/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+    render(){
+        return (
+            <div>Here is the App body</div>
+        ) 
+    }
+}
+
+export default App
